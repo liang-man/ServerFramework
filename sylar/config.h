74ec465be614014381c648e4503ab78e3ceef899
liang-man <liangman213@163.com>
2023-03-09 21:29:54 +0800

    定义类Config， 实现创建和查找日志

--- sylar/config.h
+++ sylar/config.h
@@ -5,6 +5,9 @@
 #include <sstream> // 序列化
 #include <boost/lexical_cast.hpp>   // 内存转化
 #include <string>
+#include <map>
+#include <exception>
+#include "log.h"
 
 namespace sylar {
 
@@ -60,6 +63,42 @@ public:
 private:
     T m_val;
 };
+
+// 实现创建和查找日志
+class Config {
+public:
+    typedef std::map<std::string, ConfigVarBase::ptr> ConfigVarMap;
+
+    template<class T>
+    static typename ConfigVar<T>::ptr Lookup(const std::string &name, const T &default_vale,
+            const std::string &description = "") {
+        auto tmp = Lookup<T>(name);
+        if (tmp) {
+            SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << "Lookup name=" << name << " exists";
+            return tmp;
+        }
+
+        if (name.find_first_not_of("abcdefggijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ._012345678")
+                != std::string::npos) {
+            SYLAR_LOG_ERROR(SYLAR_LOG_ROOT()) << "Lookup name invalid " << name;
+            throw std::invalid_argument(name);
+        }
+
+        typename ConfigVar<T>::ptr v(new ConfigVar<T>(name, default_vale, description));
+        s_datas[name] = v;
+    }
+
+    template<class T>
+    static typename ConfigVar<T>::ptr Lookup(const std::string &name) {
+        auto it = s_datas.find(name);
+        if (it == s_datas.end()) {
+            return nullptr;
+        }
+        return std::dynamic_pointer_cast<ConfigVar<T>>(it->second);
+    }
+private:
+    static ConfigVarMap s_datas;
+}
 
 } // namespace syla 
 
